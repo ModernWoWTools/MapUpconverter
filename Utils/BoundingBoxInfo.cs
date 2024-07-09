9d00328feb97b671edb04617d189b0fba2c11286
Martin Benjamins <ictmartin@gmail.com>
2024-07-09 21:33:30 +0200

    Use built-in structures for bounding box

--- Utils/BoundingBoxInfo.cs
+++ Utils/BoundingBoxInfo.cs
@@ -1,57 +1,35 @@
 ﻿using Newtonsoft.Json;
-using System.Runtime.CompilerServices;
+using System.Numerics;
 
 namespace MapUpconverter.Utils
 {
     public static class BoundingBoxInfo
     {
-        public static Dictionary<string, CAaBox> boundingBoxBlobDict;
+        public static Dictionary<string, Warcraft.NET.Files.Structures.BoundingBox> boundingBoxBlobDict = new();
 
         public static void Initialize(string configPath)
         {
-            boundingBoxBlobDict = JsonConvert.DeserializeObject<Dictionary<string, CAaBox>>(File.ReadAllText(configPath));
+            var frenchBlob = JsonConvert.DeserializeObject<Dictionary<string, CAaBox>>(File.ReadAllText(configPath));
+            foreach (var frenchBoundingBox in frenchBlob)
+            {
+                boundingBoxBlobDict.Add(frenchBoundingBox.Key, new()
+                {
+                    Minimum = frenchBoundingBox.Value.BottomCorner,
+                    Maximum = frenchBoundingBox.Value.TopCorner
+                });
+            }
         }
     }
 
     public struct CAaBox
     {
-        public CAaBox(C3Vector<float> inBottomCorner, C3Vector<float> inTopCorner)
+        public CAaBox(Vector3 inBottomCorner, Vector3 inTopCorner)
         {
-            this.BottomCorner = inBottomCorner;
-            this.TopCorner = inTopCorner;
+            BottomCorner = inBottomCorner;
+            TopCorner = inTopCorner;
         }
 
-        public override string ToString()
-        {
-            DefaultInterpolatedStringHandler defaultInterpolatedStringHandler = new DefaultInterpolatedStringHandler(14, 2);
-            defaultInterpolatedStringHandler.AppendLiteral("Min : ");
-            defaultInterpolatedStringHandler.AppendFormatted<C3Vector<float>>(this.BottomCorner);
-            defaultInterpolatedStringHandler.AppendLiteral(", Max : ");
-            defaultInterpolatedStringHandler.AppendFormatted<C3Vector<float>>(this.TopCorner);
-            return defaultInterpolatedStringHandler.ToStringAndClear();
-        }
-
-        public C3Vector<float> BottomCorner;
-
-        public C3Vector<float> TopCorner;
-    }
-
-    public struct C3Vector<T>
-    {
-        public T x { readonly get; set; }
-        public T y { readonly get; set; }
-        public T z { readonly get; set; }
-
-        public override string ToString()
-        {
-            DefaultInterpolatedStringHandler defaultInterpolatedStringHandler = new DefaultInterpolatedStringHandler(16, 3);
-            defaultInterpolatedStringHandler.AppendLiteral("X : ");
-            defaultInterpolatedStringHandler.AppendFormatted<T>(this.x);
-            defaultInterpolatedStringHandler.AppendLiteral(", Y : ");
-            defaultInterpolatedStringHandler.AppendFormatted<T>(this.y);
-            defaultInterpolatedStringHandler.AppendLiteral(", Z : ");
-            defaultInterpolatedStringHandler.AppendFormatted<T>(this.z);
-            return defaultInterpolatedStringHandler.ToStringAndClear();
-        }
+        public Vector3 BottomCorner;
+        public Vector3 TopCorner;
     }
 }
