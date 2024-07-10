11c1738ae1cb4397ab0394813b9323f96ee60316
Martin Benjamins <ictmartin@gmail.com>
2024-07-10 16:03:58 +0200

    Support extra files from custom-listfile.csv

--- Utils/Listfile.cs
+++ Utils/Listfile.cs
@@ -26,6 +26,21 @@
                 //NameMap[fdid] = parts[1];
                 ReverseMap[parts[1]] = fdid;
             }
+
+            var customListfilePath = Path.Combine(listfileDir, "custom-listfile.csv");
+            if (File.Exists(customListfilePath))
+            {
+                foreach (var line in File.ReadAllLines(customListfilePath))
+                {
+                    var parts = line.Split(';');
+                    if (parts.Length < 2)
+                        continue;
+
+                    var fdid = uint.Parse(parts[0]);
+                    //NameMap[fdid] = parts[1];
+                    ReverseMap[parts[1]] = fdid;
+                }
+            }
         }
     }
 }
