9d82b114fbdc2aa7e6536a1e2df8c0215f319981
Martin Benjamins <ictmartin@gmail.com>
2024-07-24 04:18:19 +0200

    Copy over maptextures to output folder as well

--- MapUpconverter/Minimaps/Minimaps.cs
+++ MapUpconverter/Minimaps/Minimaps.cs
@@ -6,33 +6,69 @@ namespace MapUpconverter.Minimaps
     {
         public static void Convert()
         {
-            if (!Directory.Exists(Path.Combine(Settings.InputDir, "textures", "minimap")))
-                return;
-
-            foreach (var file in Directory.GetFiles(Path.Combine(Settings.InputDir, "textures", "minimap"), "*.blp"))
+            if (Directory.Exists(Path.Combine(Settings.InputDir, "textures", "minimap")))
             {
-                var cleanedName = Path.GetFileNameWithoutExtension(file).ToLower().Replace(Settings.MapName + "_", "");
-                var splitName = cleanedName.Split('_');
-
-                var targetName = Path.Combine(Settings.OutputDir, "world", "minimaps", Settings.MapName, "map" + splitName[0].PadLeft(2, '0') + "_" + splitName[1].PadLeft(2, '0') + ".blp");
-                var targetDir = Path.GetDirectoryName(targetName);
-
-                if (File.Exists(targetName))
-                    continue;
-
-                if(!Directory.Exists(targetDir))
-                    Directory.CreateDirectory(targetDir!);
-
-                var currentBLP = new BLP(File.ReadAllBytes(file));
-
-                if(currentBLP.GetPixelFormat() != BLPPixelFormat.DXT1)
+                foreach (var file in Directory.GetFiles(Path.Combine(Settings.InputDir, "textures", "minimap"), "*.blp"))
                 {
-                    var newBLP = new BLP(currentBLP.GetMipMap(0), BLPPixelFormat.DXT1, false);
-                    File.WriteAllBytes(targetName!, newBLP.Serialize());
+                    if (!Path.GetFileNameWithoutExtension(file).StartsWith(Settings.MapName + "_", StringComparison.CurrentCultureIgnoreCase))
+                        continue;
+
+                    var cleanedName = Path.GetFileNameWithoutExtension(file).ToLower().Replace(Settings.MapName.ToLower() + "_", "");
+                    var splitName = cleanedName.Split('_');
+
+                    var targetName = Path.Combine(Settings.OutputDir, "world", "minimaps", Settings.MapName, "map" + splitName[0].PadLeft(2, '0') + "_" + splitName[1].PadLeft(2, '0') + ".blp");
+                    var targetDir = Path.GetDirectoryName(targetName);
+
+                    if (!Directory.Exists(targetDir))
+                        Directory.CreateDirectory(targetDir!);
+
+                    var currentBLP = new BLP(File.ReadAllBytes(file));
+
+                    if (currentBLP.GetPixelFormat() != BLPPixelFormat.DXT1)
+                    {
+                        var newBLP = new BLP(currentBLP.GetMipMap(0), BLPPixelFormat.DXT1, false);
+                        File.WriteAllBytes(targetName!, newBLP.Serialize());
+                    }
+                    else
+                    {
+                        File.Copy(file, targetName!, true);
+                    }
                 }
-                else
+            }
+
+            if (Directory.Exists(Path.Combine(Settings.InputDir, "textures", "maptextures")))
+            {
+                foreach (var file in Directory.GetFiles(Path.Combine(Settings.InputDir, "textures", "maptextures"), "*.blp"))
                 {
-                    File.Copy(file, targetName!);
+                    if (!Path.GetFileNameWithoutExtension(file).StartsWith(Settings.MapName + "_", StringComparison.CurrentCultureIgnoreCase))
+                        continue;
+
+                    var cleanedName = Path.GetFileNameWithoutExtension(file).ToLower().Replace(Settings.MapName.ToLower() + "_", "");
+                    var splitName = cleanedName.Split('_');
+
+                    var targetName = Path.Combine(Settings.OutputDir, "world", "maptextures", Settings.MapName, Settings.MapName + "_" + splitName[0].PadLeft(2, '0') + "_" + splitName[1].PadLeft(2, '0') + ".blp");
+
+                    if (splitName.Length == 3 && splitName[2] == "n")
+                    {
+                        targetName = Path.Combine(Settings.OutputDir, "world", "maptextures", Settings.MapName, Settings.MapName + "_" + splitName[0].PadLeft(2, '0') + "_" + splitName[1].PadLeft(2, '0') + "_n.blp");
+                    }
+
+                    var targetDir = Path.GetDirectoryName(targetName);
+
+                    if (!Directory.Exists(targetDir))
+                        Directory.CreateDirectory(targetDir!);
+
+                    var currentBLP = new BLP(File.ReadAllBytes(file));
+
+                    if (currentBLP.GetPixelFormat() != BLPPixelFormat.DXT5)
+                    {
+                        var newBLP = new BLP(currentBLP.GetMipMap(0), BLPPixelFormat.DXT5, true);
+                        File.WriteAllBytes(targetName!, newBLP.Serialize());
+                    }
+                    else
+                    {
+                        File.Copy(file, targetName!, true);
+                    }
                 }
             }
         }
